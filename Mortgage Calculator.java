615203a348228787615290334a5e14df19baa0bc
nihalnaf <86746598+nihalnaf@users.noreply.github.com>
2021-07-27 17:13:48 -0400

    Update Mortgage Calculator.java

--- Mortgage Calculator.java
+++ Mortgage Calculator.java
@@ -6,16 +6,20 @@ import java.util.Scanner;
 class Main {
 
     public static void main(String[] args) {
-
+        
+        //CONSTANTS
         final byte MONTHS_IN_YEARS = 12;
         final byte PERCENT = 100;
 
+        //Create scanner object
         Scanner scanner = new Scanner(System.in);
+        //Declare variables
         float principal = 0;
         float monthlyRate = 0;
         byte years = 0;
         int numberOfPayments = 0;
 
+        //While loop for user input for principal payment
         while (true) {
             System.out.print("Principal ($1K - $1M): ");
             principal = scanner.nextFloat();
@@ -25,6 +29,7 @@ class Main {
             System.out.print("Enter a value between 1000 and 1000000: ");
         }
 
+        //While loop for user input for annual interest rate
         while (true) {
             System.out.print("Annual Interest Rate: ");
             float annualRate = scanner.nextFloat();
@@ -35,6 +40,7 @@ class Main {
             System.out.print("Enter a value between 1 and 30: ");
         }
 
+        //While loop for user input for period of years
         while (true) {
             System.out.print("Period (Years): ");
             years = scanner.nextByte();
@@ -44,9 +50,10 @@ class Main {
             }
             System.out.print("Enter a value between 1 and 30: ");
         }
-
+        
+        //Calculate mortgage
         double mortgage = principal * (monthlyRate * Math.pow(1 + monthlyRate, numberOfPayments)) / (Math.pow(1 + monthlyRate, numberOfPayments) - 1);
-
+        //Print out mortgage in a currency format
         System.out.println("Mortgage: " + NumberFormat.getCurrencyInstance().format(mortgage));
 
 
